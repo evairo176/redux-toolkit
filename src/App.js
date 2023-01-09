ef9a007eb3d903a3b8ac8b7df724e733e61197a2
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-01-09 11:06:03 +0700

    feat: update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import {
+  decrement,
+  increaseAmount,
+  increment,
+} from "./redux/slices/counterSlices";
 
 function App() {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state?.counter);
+  console.log(counter);
   return (
     <div className="App">
       <h1>Redux Toolkit Counter</h1>
+      <h1>Counter:{counter?.value}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button>Increase Amount</button>
     </div>
   );
 }
